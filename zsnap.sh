ab310546928040981e5ae6b34cde01a47d4d32c1
deajan <ozy@netpower.fr>
2016-03-14 22:45:04 +0100

    Adapted coding style

--- zsnap.sh
+++ zsnap.sh
@@ -278,16 +278,13 @@ function LoadConfigFile {
 #### MINIMAL-FUNCTION-SET END ####
 
 
-function TrapStop
-{
+function TrapStop {
 	Logger "/!\ Manual exit of zsnap script. zfs snapshots may not be mounted." "WARN"
 	exit 1
 }
 
-function TrapQuit
-{
-	if [ $ERROR_ALERT -ne 0 ]
-	then
+function TrapQuit {
+	if [ $ERROR_ALERT -ne 0 ]; then
 		SendAlert
 		Logger "Zsnap script finished with errors." "ERROR"
 	else
@@ -298,146 +295,120 @@ function TrapQuit
 	fi
 }
 
-function CheckEnvironment
-{
+function CheckEnvironment {
 	if ! type -p zfs > /dev/null 2>&1
 	then
 		Logger "zfs not present. zsnap cannot work." "CRITICAL"
-		exit 1
+		Usage
 	fi
 
 	if ! type -p zpool > /dev/null 2>&1
 	then
 		Logger "zpool not present. zsnap cannot work." "CRITICAL"
-		exit 1
+		Usage
 	fi
 }
 
 # Count number of snapshots of $ZFS_VOLUME
-function CountSnaps
-{
+function CountSnaps {
 	SNAP_COUNT=$($(which zfs) list -t snapshot -H | grep "^$ZFS_VOLUME@" | wc -l)
-	if [ $? != 0 ]
-	then
+	if [ $? != 0 ]; then
 		SNAP_COUNT=0
 	fi
-	if [ "$_VERBOSE" -ne 0 ]
-	then
+	if [ "$_VERBOSE" -ne 0 ]; then
 		Logger "CountSnaps: There are $SNAP_COUNT snapshots in $ZFS_VOLUME" "NOTICE"
 		return 0
 	fi
 }
 
 # Destroys a snapshot given as argument
-function DestroySnap
-{
+function DestroySnap {
 	if [ "$USE_SHADOW_COPY2" != "yes" ]
 	then
 		mountpoint=$(mount | grep $1 | cut -d' ' -f3)
-		if [ "$mountpoint" != "" ]
-		then
+		if [ "$mountpoint" != "" ]; then
 			umount "$mountpoint"
-			if [ $? != 0 ]
-			then
+			if [ $? != 0 ]; then
 				Logger "DestroySnap: Cannot unmount snapshot $1 from $mountpoint" "ERROR"
 				return 1
-			elif [ "$_VERBOSE" -ne 0 ]
-			then
+			elif [ "$_VERBOSE" -ne 0 ]; then
 				Logger "DestroySnap: Snapshot $1 unmounted from $mountpoint" "NOTICE"
 			fi
 		fi
 	fi
 
 	$(which zfs) destroy $1
-	if [ $? != 0 ]
-	then
+	if [ $? != 0 ]; then
 		Logger "DestroySnap: Cannot destroy snapshot $1" "ERROR"
 		return 1
 	else
 	Logger "DestroySnap: Snapshot $1 destroyed" "NOTICE"
 	fi
 
-	if [ -d $mountpoint ] && [ "$mountpoint" != "" ]
-	then
+	if [ -d $mountpoint ] && [ "$mountpoint" != "" ]; then
 		rm -r $mountpoint
-		if [ $? != 0 ]
-		then
+		if [ $? != 0 ]; then
 			Logger "DestroySnap: Cannot delete mountpoint $mountpoint" "ERROR"
 			return 1
-		elif [ "$_VERBOSE" -ne 0 ]
-		then
+		elif [ "$_VERBOSE" -ne 0 ]; then
 			Logger "DestroySnap: Mountpoint $mountpoint deleted" "NOTICE"
 		fi
 	fi
 }
 
 # Destroys oldest snapshot, or destroys all snapshots in volume if argumennt "all" is given
-function DestroySnaps
-{
+function DestroySnaps {
 	arg="$1"
 	destroycount=0
 	for snap in $($(which zfs) list -t snapshot -H | grep "^$ZFS_VOLUME@" | cut -f1)
 	do
 		DestroySnap $snap
 		destroycount=$(($destroycount + 1))
-		if [ "$arg" == "" ]
-		then
+		if [ "$arg" == "" ]; then
 			break;
-		elif [ "$arg" == "all" ]
-		then
+		elif [ "$arg" == "all" ]; then
 			break;
-		elif [ $destroycount -ge "$arg" ]
-		then
+		elif [ $destroycount -ge "$arg" ]; then
 			break;
 		fi
 	done
 }
 
 # Gets disk usage of zpool $ZFS_POOL
-function GetZvolUsage
-{
+function GetZvolUsage {
 	USED_SPACE=$($(which zpool) list -H | grep $ZFS_POOL | cut -f5 | cut -d'%' -f1)
 	## Added support for new zfsonlinux 0.6.4 zpool output
-	if [ "$USED_SPACE" == "-" ]
-	then
+	if [ "$USED_SPACE" == "-" ]; then
 		USED_SPACE=$($(which zpool) list -H | grep $ZFS_POOL | cut -f7 | cut -d'%' -f1)
 	fi
 
-	if [ $? != 0 ]
-	then
+	if [ $? != 0 ]; then
 		Logger "GetZvolUsage: Cannot get disk usage of pool $ZFS_POOL" "ERROR"
 		return 1
-	elif [ "$_VERBOSE" -ne 0 ]
-	then
+	elif [ "$_VERBOSE" -ne 0 ]; then
 		Logger "GetZvolUsage: Disk usage of $ZFS_POOL = $USED_SPACE %" "NOTICE"
 	fi
 }
 
 # Mounts all current snapshots of $ZFS_VOLUME in samba vfs shadow_copy compatible format
-function MountSnaps
-{
+function MountSnaps {
 	zvol_mountpoint=$($(which zfs) get mountpoint $ZFS_VOLUME -H | cut -f3)
 	for snap in $($(which zfs) list -t snapshot -H | grep "^$ZFS_VOLUME@" | cut -f1)
 	do
 		snap_mountpoint=$(echo $snap | cut -d'@' -f2)
-		if [ "$(mount | grep $snap_mountpoint | wc -l)" -eq 0 ]
-		then
+		if [ "$(mount | grep $snap_mountpoint | wc -l)" -eq 0 ]; then
 			mkdir -p $zvol_mountpoint/@GMT-$snap_mountpoint
-			if [ $? != 0 ]
-			then
+			if [ $? != 0 ]; then
 				Logger "MountSnaps: Cannot create mountpoint directory $zvol_mountpoint/$snap_mountpoint" "ERROR"
 				return 1
-			elif [ "$_VERBOSE" -ne 0 ]
-			then
+			elif [ "$_VERBOSE" -ne 0 ]; then
 				Logger "MountSnaps: Created mountpoint directory $zvol_mountpoint/@GMT-$snap_mountpoint" "NOTICE"
 			fi
 			mount -t zfs $snap $zvol_mountpoint/@GMT-$snap_mountpoint
-			if [ $? != 0 ]
-			then
+			if [ $? != 0 ]; then
 				Logger "MountSnaps: Cannot mount $snap on $zvol_mountpoint/@GMT-$snap_mountpoint" "ERROR"
 				return 1
-			elif [ "$_VERBOSE" -ne 0 ]
-			then
+			elif [ "$_VERBOSE" -ne 0 ]; then
 				Logger "MountSnaps: Snapshot $snap mounted on $zvol_mountpoint/@GMT-$snap_mountpoint" "NOTICE"
 			fi
 		fi
@@ -445,16 +416,14 @@ function MountSnaps
 }
 
 # Unmounts all snapshots and deletes its mountpoint directories
-function UnmountSnaps
-{
+function UnmountSnaps {
 	for mountpoint in $(mount | grep "^$ZFS_VOLUME@" | cut -d' ' -f3)
 	do
 		umount "$mountpoint"
 		if [ $? != 0 ]
 		then
 			Logger "UnmountSnaps: Cannot unmount $mountpoint" "ERROR"
-		elif [ "$_VERBOSE" -ne 0 ]
-		then
+		elif [ "$_VERBOSE" -ne 0 ]; then
 			Logger "UnmountSnaps: $mountpoint unmounted" "NOTICE"
 		fi
 
@@ -462,29 +431,24 @@ function UnmountSnaps
 		if [ $? != 0 ]
 		then
 			Logger "UnmountSnaps: Cannot delete mountpoint $mountpoint" "ERROR"
-		elif [ "$_VERBOSE" -ne 0 ]
-		then
+		elif [ "$_VERBOSE" -ne 0 ]; then
 			Logger "UnmountSnaps: Mountpoint $mountpoint deleted" "NOTICE"
 		fi
 	done
 }
 
 # Creates a new snapshot. Unmounts snapshots before creation and remounts them afterwards so snapshot mountpoints won't be snapshotted
-function CreateSnap
-{
-	if [ "$USE_SHADOW_COPY2" == "no" ]
-	then
+function CreateSnap {
+	if [ "$USE_SHADOW_COPY2" == "no" ]; then
 		UnmountSnaps
 	fi
-	if [ "$USE_UTC" != "no" ]
-	then
+	if [ "$USE_UTC" != "no" ]; then
 		SNAP_TIME=$(date -u +%Y.%m.%d-%H.%M.%S)
 	else
 		SNAP_TIME=$(date +%Y.%m.%d-%H.%M.%S)
 	fi
 	$(which zfs) snapshot $ZFS_VOLUME@$SNAP_TIME
-	if [ $? != 0 ]
-	then
+	if [ $? != 0 ]; then
 		Logger "CreateSnap: Cannot create snapshot $ZFS_VOLUME@$SNAP_TIME" "ERROR"
 		return 1
 	fi
@@ -496,13 +460,11 @@ function CreateSnap
 }
 
 # Does the same as CreateSnap, but verifies enforcing parameters first
-function VerifyParamsAndCreateSnap
-{
+function VerifyParamsAndCreateSnap {
 	max_space_reached=0
 	GetZvolUsage
 	CountSnaps
-	if [ "$_VERBOSE" -ne 0 ]
-	then
+	if [ "$_VERBOSE" -ne 0 ]; then
 		Logger "There are currently $SNAP_COUNT snapshots on volume $ZFS_VOLUME for $USED_SPACE % disk usage" "NOTICE"
 	fi
 
@@ -520,29 +482,25 @@ function VerifyParamsAndCreateSnap
 		max_space_reached=1
 	done
 
-	if [ "$_VERBOSE" -ne 0 ]
-	then
+	if [ "$_VERBOSE" -ne 0 ]; then
 		Logger "After enforcing, there are $SNAP_COUNT snapshots on volume $ZFS_VOLUME for $USED_SPACE % disk usage" "NOTICE"
 	fi
 
-	if [ $max_space_reached -eq 1 ]
-	then
+	if [ $max_space_reached -eq 1 ]; then
 		Logger "$MAX_SPACE disk usage was reached." "WARN"
 	fi
 
 	CreateSnap
 }
 
-function Status
-{
+function Status {
 	echo "zsnap $ZSNAP_VERSION status"
 	echo ""
 	GetZvolUsage
 	CountSnaps
 	echo "Number of snapshots (min < actual < max): $MIN_SNAPSHOTS < $SNAP_COUNT < $MAX_SNAPSHOTS"
 	echo "Disk usage: $ZFS_POOL: $USED_SPACE %"
-	if [ "$_VERBOSE" -ne 0 ]
-	then
+	if [ "$_VERBOSE" -ne 0 ]; then
 		echo ""
 		echo "Snapshot list"
 		for snap in $($(which zfs) list -t snapshot -H | grep "^$ZFS_VOLUME@" | cut -f1)
@@ -552,8 +510,7 @@ function Status
 	fi
 }
 
-function Init
-{
+function Init {
 	set -o pipefail
 	set -o errtrace
 
@@ -568,8 +525,7 @@ function Init
 	fi
 }
 
-function Usage
-{
+function Usage {
 	echo "$PROGRAM $PROGRAM_VERSION $PROGRAM_BUILD"
 	echo "$AUTHOR"
 	echo "$CONTACT"
@@ -609,8 +565,7 @@ do
 done
 
 CheckEnvironment
-if [ $? == 0 ]
-then
+if [ $? == 0 ]; then
 	if [ "$1" != "" ]
 	then
 		LoadConfigFile "$1"
@@ -640,10 +595,8 @@ then
 				UnmountSnaps
 				;;
 				destroy)
-				if [ "$3" != "" ]
-				then
-					if [[ "$3" == *"@"* ]]
-					then
+				if [ "$3" != "" ]; then
+					if [[ "$3" == *"@"* ]]; then
 						DestroySnap "$3"
 					else
 						DestroySnaps "$3"
